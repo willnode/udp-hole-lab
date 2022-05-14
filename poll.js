941324dd9c703385dc5c4ab8ef5f3cecf2dc8337
Wildan M <willnode@wellosoft.net>
2022-05-15 05:06:35 +0700

    conn nov

--- poll.js
+++ poll.js
@@ -5,8 +5,9 @@ const server = dgram.createSocket('udp4');
 server.on('message', (msg, rinfo) => {
     console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
   });
-  
-server.connect('41234', '127.0.0.1', () => {
+
+server.connect('41234', '192.168.43.167', () => {
+    console.log('connected to server', server.remoteAddress());
     server.send('LIST')
     setInterval(() => {
         server.send('p')
